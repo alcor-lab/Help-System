1d2cc19cbd105c272a7fb9b7716cbae9054ce639
Valsamis Ntouskos <ntouskos@diag.uniroma1.it>
2019-06-09 23:55:56 +0100

    Revert "Merge branch 'master' into ms3"
    This reverts commit 52344afb8984bcc9e708250d5f5f4cf5edb615f5, reversing
    changes made to c8cd03e7f7e7e9e1f6dd32204990ceca1fa1700e.

--- Libraries/help_recognition/config.py
+++ Libraries/help_recognition/config.py
@@ -7,11 +7,10 @@ frames_per_step = 6
 input_channels = 7
 seq_len = 4
 enc_fc_1 = 4000
-enc_fc_2 = 1500
-lstm_units = 800
-pre_class = 400
-encoder_lstm_layers = 2*[lstm_units]
+enc_fc_2 = int(enc_fc_1 / 2)
+lstm_units = int(enc_fc_2 / 2)
+pre_class = int(lstm_units / 2)
+encoder_lstm_layers = 3*[lstm_units]
 hidden_states_dim = lstm_units
-vocab_len = 34
 
 test_video_path = 'dataset/Video/kit_dataset/TEST_VIDEOS'
